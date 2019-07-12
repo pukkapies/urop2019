93d8f14939a0076e9d38dfea9fefa0c506835a6f
Davide Gallo <dvdgallo98@gmail.com>
2019-07-12 13:43:42 +0100

    Split data_wrangle into module and script

--- modules/data_wrangle.py
+++ modules/data_wrangle.py
@@ -151,26 +151,3 @@ def df_purge_duplicates(track_df: pd.DataFrame, info_file:str, mode: str = 'sing
     
     else:
         raise KeyError
-
-
-if __name__ == '__main__':
-    # convert the (desired columns in the) HDF5 summary file as a dataframe
-    df_summary = extract_ids_from_summary(PATH_TO_H5)
-        
-    # search for MP3 tracks through the MP3_ROOT_DIR folder
-    df = find_tracks_with_7dids(MP3_ROOT_DIR)
-    
-    # create a new dataframe with the metadata for the tracks we actually have on the server
-    df = df_merge(df_summary, df)
-        
-    # discard mismatches
-    df = df_purge_mismatches(df, PATH_TO_MISMATCHES_TXT)
-    
-    # discard duplicates
-    # df = df_purge_duplicates(df, PATH_TO_DUPLICATES_TXT)
-
-    # save output
-    output = 'ultimate_csv.csv'
-    output_path = os.path.join(OUTPUT_DIR, output)
-
-    our_df.to_csv(output_path, header=False, index=False)
