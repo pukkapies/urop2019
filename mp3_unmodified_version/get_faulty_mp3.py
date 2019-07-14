d22346133e4eb9235ae462599d958c1c8ba6f1c4
Chon Wai Ho (Aden) <48314854+hcw1026@users.noreply.github.com>
2019-07-14 15:51:37 +0100

    Added doc and combined some scripts

--- mp3_unmodified_version/get_faulty_mp3.py
+++ mp3_unmodified_version/get_faulty_mp3.py
@@ -4,12 +4,46 @@ Created on Mon Jul  8 22:19:15 2019
 
 @author: MacBook Pro
 """
-
 '''
-To use this function, you need to download the 'ultimate_csv_size.csv'. This
-function takes the csv file and return the list of track_7digitalid of tracks
-that either have zero size or cannot be opened.
-If you want to return track_id instead, set track_7digitalid=False
+Note
+----
+
+
+Functions
+---------
+- get_faulty_mp3              DEPRECATED
+
+- check_silence               Interpret the csv obtained from no_sound.py and
+                              add extra columns to the output csv.
+                              
+- filter_trim_length          Return the track_id of tracks that satisfy the 
+                              condition: 
+                              duration after trimming >= min_duration.
+                              
+- filter_tot_silence_duration Return the track_id of tracks that satisfy the 
+                              condition: 
+                              total length of mid-silent duration 
+                              <= max_duration.
+                              
+- filter_max_silence_duration Return the track_id of tracks that satisfy the 
+                              condition: 
+                              the maximum length amongst the individual 
+                              mid-silent sections <= max_duration.
+
+
+
+Glossary
+--------
+
+    trim:
+        The total duration of tracks excluding the starting and ending section 
+        if they are silent respectively.
+        
+    mid-silent section:
+        Any section in the track which is silent, but it is neither the 
+        starting silent section nor the ending silent section.
+        
+
 '''
 
 
@@ -21,7 +55,7 @@ import no_sound
 #path='D://UROP/millionsongsubset_full/MillionSongSubset'
 def get_faulty_mp3(path, file='ultimate_csv_size.csv', track_id=True):
     '''
-    set track_id=False if you want it to return track_7digitalid instead
+    DEPRECATED
     '''
     
     DIR = os.path.join(path, file)
@@ -44,6 +78,60 @@ def get_faulty_mp3(path, file='ultimate_csv_size.csv', track_id=True):
 
 
 def check_silence(path='/home/aden/urop2019', file='pre_no_sound.csv'):
+    
+    '''
+    Parameters
+    ----------
+    path: str
+        The path where the csv returned by the function pre_no_sound is saved.
+    
+    file: str
+        The saved name of the csv returned by the function pre_no_sound.
+        
+    Returns
+    -------
+    csv:
+        The csv is saved under the path provided in the Parameters. This
+        function extracts features from the npz files saved by the no_sound.py.
+        It combines several new columns based on the features extracted and
+        appended to the dataframe provided by the Parameters and return a new 
+        csv with the extra columns.
+        
+        
+        Extra columns:
+        'start': float
+            The start time (s) of non-silent section of tracks.
+            
+        'end': float
+            The end time (s) of non-silent section of tracks.
+            
+        'silent_details': list
+            A list of tuples, and each tuple contains 2
+            elements of the form (a,b). Each tuple represents a silent section 
+            (after trimming), where a, b are the start and end time of the 
+            section respectively.
+            
+        'mid_silence_length': float
+            The sum of the length of all the mid-silent sections.
+            
+        'lengths(after_trim)': float
+            The length of track after trimming.
+            
+        'non_silence_length': float
+            The total duration of the non-silent sections of the track.
+            
+        'perc_after_trim': float
+            The percentage of the track which is non-silent after trimming.
+        
+        'silence_duration': list
+                length of each mid-silent section.
+                
+        'max_duration': float
+                maximum length of individual mid-silent sections from the 
+                'silence_duration' column.
+    
+                
+    '''
     
     DIR = os.path.join(path, file)
     df = pd.read_csv(DIR)
@@ -97,11 +185,120 @@ def check_silence(path='/home/aden/urop2019', file='pre_no_sound.csv'):
     - df.loc[:,'mid_silence_length']
     df.loc[:,'perc_after_trim'] = (df.loc[:,'non_silence_length'] \
           / df.loc[:, 'lengths(after_trim)'])*100
+          
+    def get_duration(x):
+        LIST = []
+        for i in x:
+            if i != None:
+                _ = i[1] - i[0]
+                LIST.append(_)
+        return LIST
+
+    # get lengths of silent sections:
+    df['silence_duration'] = df.silence_detail.apply(get_duration)
     
-    df.to_csv(os.path.join(path, 'ultimate_csv_size2.csv'))
+    def get_max(x):
+        if x:
+            return np.max(x)
+        else:
+            return None
+    
+    #get maximum lengths of individual silent sections:
+    df['max_duration'] = df.silence_duration.apply(get_max)
+    
+    df.to_csv(os.path.join(path, 'ultimate_csv_size2.csv'), index=False)
     
     
-        
+
+
+def filter_trim_length(df, min_duration):
+    '''
+    Parameters
+    ---------
+    df: pd.DataFrame
+        The datafram from the function filter_mp3.
+    
+    min_duration: float
+        If the length of tracks AFTER TRIMMING >= min_duration, keep the track 
+        and its track_id is returned.
+    
+    
+    Returns
+    ------
+    list
+        The track_id of tracks that satisfy the condition: 
+            duration after trimming >= min_duration.
+    
+    '''
+    
+    ID = df[df['lengths(after_trim)'] >= min_duration]
+    return ID.track_id.tolist()
+
+
+
+def filter_tot_silence_duration(df, max_duration):
+    '''
+    Parameters
+    ---------
+    df: pd.DataFrame
+        The datafram from the function filter_mp3.
+    
+    max_duration: float
+        If the sum of the length of mid-silent sections <= min_duration, 
+        keep the track and its track_id is returned.
+    
+    
+    Returns
+    ------
+    list
+        The track_id of tracks that satisfy the condition: 
+            total length of mid-silent duration <= max_duration.
+    
+    '''
+    
+    ID = df[(df['mid_silence_length'] <= max_duration)]
+    return ID.track_id.tolist()
+
+
+#This functions takes the dataframe and max_duration of an individual mid-silent section (e.g. 1s)
+#return the track_id that SATISFIES the condition -- the maximum length of an indiviual mid-silent section <= max_duration
+def filter_max_silence_duration(df, max_duration):
+    '''
+    Parameters
+    ---------
+    df: pd.DataFrame
+        The datafram from the function filter_mp3.
+    
+    max_duration: float
+        If the maximum length amongst the individual mid-silent sections 
+        <= max_duration, keep the track and its track_id is returned.
+    
+    
+    Returns
+    ------
+    list
+        The track_id of tracks that satisfy the condition: 
+            the maximum length amongst the individual mid-silent sections 
+            <= max_duration.
+    
+    '''
+    
+    ID = df[(df['max_duration'] <= max_duration) 
+            | (np.isnan(df['max_duration']))]
+    return ID.track_id.tolist()
+
+
+'''
+EXAMPLE
+-------
+
+a = filter_tot_silence_duration(df, 1)
+b = filter_max_silence_duration(df, 1)
+c = filter_trim_length(df, 15)
+
+list of track_id: set(a).intersection(set(b), set(c))
+
+'''
         
         
     
--- mp3_unmodified_version/no_sound.py
+++ mp3_unmodified_version/no_sound.py
@@ -5,34 +5,112 @@ Created on Tue Jul  9 23:21:48 2019
 @author: MacBook Pro
 """
 
+'''
+Notes
+-----
+
+
+Functions
+---------
+- create_folder_structure  This copies the folder structure of how the mp3 
+                           files are saved and produce the same folder 
+                           structure under a new directory, which will be used 
+                           to save the converted numpy arrays later.
+                           
+- no_sound                 Save three numpy arrays per track:
+                           1. loaded mp3 arrays    2. sampling rate 
+                           3. start and end positoin of arrays in 1 when volume
+                              of track is above 60dB (non-silent)
+                           See the librosa documentation on 
+                           librosa.effect.split for more details.
+                           
+                              
+- count                    Return the number of mp3 files that have been saved
+                           as npz files. Return the path of tracks that have
+                           not been converted yet if final_check mode is 
+                           enabled.
+                           
+- zip_correction           Searches for zip files errors and is an error 
+                           handling tool used in get_faulty_mp3.py
+
+
+'''
+
+
 import librosa
 import pandas as pd
 import time
 import numpy as np
 import os
 
-def no_sound(start, end):
+MP3_ROOT_DIR = '/srv/data/msd/7digital/'
+NUMPY_ROOT_DIR = '/srv/data/urop/7digital_numpy/'
 
-    ult_path = '~/urop2019/pre_no_sound.csv'
+def create_folder_structure():
+    for dirpath, dirnames, filenames in os.walk(MP3_ROOT_DIR):
+        structure = os.path.join(NUMPY_ROOT_DIR, dirpath[len(MP3_ROOT_DIR):])
+        if not os.path.isdir(structure):
+            os.mkdir(structure)
+        else:
+            print("Folder does already exits!")
+
+
 
 #ult_path = 'D://UROP/millionsongsubset_full/MillionSongSubset/ultimate_csv.csv'
+def no_sound(start, end, ult_path='~/urop2019/pre_no_sound.csv'):
+    
+    '''
+    Parameters
+    ----------
+    
+        start: int
+            The index of starting point in the pre_no_sound.csv.
+            
+        end: int
+            The index of ending point in the pre_no_sound.csv.
+            
+        ult_path: str
+            The directory of the csv that will be using to create the npz file.
+            Default--pre_no_sound.csv
+         
+    
+    Returns
+    -------
+    
+    npz files:
+        The npz file is of the form ['array', 'sr', 'split']
+        'array': 
+            The loaded mp3 files in numpy-array form by library -- librosa.
+            The array represent the mp3s in its original sampling rate, and 
+            multi-channel is preserved. (each row represents one channel)
+            
+        'sr':
+            The sampling rate of the mp3 file.
+            
+        'split':
+            All the sections of the track which is non-silent (>=60dB). The
+            information is saved in the form: n*2 numpy.ndarray, and each row
+            represent one section -- starting position and ending position of 
+            array respectively.
+    
+    '''
+ 
 
     df = pd.read_csv(ult_path)
 
     paths = df.path.tolist()[start:end]
 
 
-#librosa.core.load('D://UROP/tracks/4019379.clip.mp3')
 
     for i, path in enumerate(paths):
-        path_np = '/srv/data/urop/7digital_numpy'+path[:-9]
+        path_np = NUMPY_ROOT_DIR[:-1] +path[:-9]
         start_time = time.time()
         if os.path.isfile(path_np+'.npz'):
             print('Already Exist')
         
         else:
             
-            _ = '/srv/data/msd/7digital'+path
+            _ = MP3_ROOT_DIR[:-1] +path
     
             array, sr = librosa.core.load(_, sr=None, mono=False)
     
@@ -46,15 +124,37 @@ def no_sound(start, end):
             print(i)
                 
 
-def count(final_check=False):
-    ult_path = '~/urop2019/pre_no_sound.csv'
+def count(final_check=False, ult_path='~/urop2019/pre_no_sound.csv'):
+    
+    '''
+    Parameters
+    ----------
+    final_check: bool
+        final check mode.
+        
+    ult_path: str
+        The directory of the csv that was used to create the npz file.
+        Default--pre_no_sound.csv
+        
+    Returns
+    -------
+    LIST: list
+        If it is in final check mode, it returns the list of path of the tracks
+        whose mp3 has not been loaded and saved to numpy array
+        
+    Counter: int
+        The number of mp3s that have been loaded and saved as numpy array.
+        
+    
+    '''
+    
     df = pd.read_csv(ult_path)
     paths = df.path.tolist()
     counter = 0
     LIST=[]
      
     for i, path in enumerate(paths):
-        path_np = '/srv/data/urop/7digital_numpy'+path[:-9]
+        path_np = NUMPY_ROOT_DIR[:-1] +path[:-9]
         if os.path.isfile(path_np+'.npz'):
             counter+=1
         else:
@@ -71,9 +171,25 @@ def count(final_check=False):
         
         
 def zip_correction(trackid):
+    '''
+    NEED CHANGES IN PATHS?
+    
+    Parameters
+    ----------
+    trakid: int
+        The track_id of the track.
+        
+    Returns
+    -------
+    npz file:
+        The npz file described in the function no_sound()
+    
+    '''
+    
+    
     path = '/'+str(trackid)[0]+'/'+str(trackid)[1]+'/'+str(trackid)+'.clip.mp3'
-    path_np = '/srv/data/urop/7digital_numpy'+path[:-9]
-    _ = '/srv/data/msd/7digital'+path
+    path_np = NUMPY_ROOT_DIR[:-1] +path[:-9]
+    _ = MP3_ROOT_DIR[:-1] +path
     array, sr = librosa.core.load(_, sr=None, mono=False)
     array_mono = librosa.core.to_mono(array)
     array_split = librosa.effects.split(array_mono)
