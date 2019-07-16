3567fd55c338275d936cf97a4ffed33585faa814
Anagh Malik <anaghmalik@gmail.com>
2019-07-16 09:02:28 +0200

    fixed minor errors

--- scripts/log_mel_spectogram.py
+++ scripts/log_mel_spectogram.py
@@ -11,10 +11,11 @@ import pandas as pd
 
 root_dir = '/srv/data/urop/7digital_numpy/'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     # Looping through all of the directories containing .npz files
     for i in range(10):
         for j in range(10):
+
             dir = os.path.join(root_dir, str(i), str(j)) # Current directory
             # Loop through files in directory
             for file_name in os.listdir(dir):
@@ -27,16 +28,18 @@ if __name__ = '__main__':
                 spectrogram = librosa.feature.melspectrogram(
                                     file['array'][0], int(file['sr'])) 
                 shape = spectrogram.shape
-                array = np.empty(file['array'].shape[0], shape[0], shape[1])
+                array = np.empty((file['array'].shape[0], shape[0], shape[1]))
                 array[0] = spectrogram
 
                 # Get spectrograms from remaining channels
                 n_channels = file['array'].shape[0]
                 for i in range(1, n_channels):
-                    array[i] = librosa.feature.melspectrogram(
-                                        file['array'][i], int(file['sr']))
+                    array[i] = librosa.feature.melspectrogram(file['array'][i], int(file['sr']))
 
                 # Turning all channels into log-mel spectograms simultaneously
                 array = np.log(array)
 
                 # TODO: Output this to where????
+
+
+
