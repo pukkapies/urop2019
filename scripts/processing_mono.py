9b6dcfb99e1e20867fb3a67ed73395aca007a365
Calle Sönne <calle.sonne@gmail.com>
2019-07-17 16:30:53 +0100

    Very rough outline of script

--- scripts/processing_mono.py
+++ scripts/processing_mono.py
@@ -1,11 +1,16 @@
-'''
+''' Script for converting waveforms into spectrograms and saving as TFRecords file
 
 '''
+
+# TODO: Maybe "recycle" this script to also create TFRecords from raw waveforms.
+# Could utilize arguments for this
 
 import os
 
 import librosa
 import numpy as np
+import tensorflow as tf
+
 from .modules import query_lastfm as q_fm
 from .modules import query_msd_summary as q_msd
 
@@ -13,55 +18,72 @@ from .modules import query_msd_summary as q_msd
 root_dir = '/srv/data/urop/7digital_numpy/'
 TAGS = [] # Allowed tags
 
-def get_tags_7digitalid(id):
-    ''' Gets tags using 7digitalid
-    
-    Parameters
-    ----------
-    id : str or int 
-        The 7digital id.
-
-    Returns
-    -------
-    list of tags
-    '''
-
-    return q_fm.get_tags(q_msd.get_trackid_from_7digitalid(id))
-
-
 def encode_tags(tags):
     ''' Encodes tags for the TFRecords file '''
+    # TODO
     return
 
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 if __name__ == '__main__':
-    # Looping through all of the directories containing .npz files
-    for i in range(10):
-        for j in range(10):
+    with tf.python_io.TFRecordWriter(tf_filename) as writer: # TODO: Decide filename 
+        # Looping through all of the directories containing .npz files
+        for i in range(10):
+            for j in range(10):
 
-            dir = os.path.join(root_dir, str(i), str(j)) # Current directory
-            # Loop through files in directory
-            for file_name in os.listdir(dir):
+                dir = os.path.join(root_dir, str(i), str(j)) # Current directory
+                # Loop through files in directory
+                for file_name in os.listdir(dir):
 
-                path = os.path.join(dir, file_name)
-                id = file_name[:-4]
-                                
-                file = np.load(path)
-                sr = int(file['sr']) 
+                    path = os.path.join(dir, file_name)
+                    # TODO: Get TID using Davides+Adens new database
+                    # id_7digital = file_name[:-4]
+                    tid = ""
+                                    
+                    file = np.load(path)
+                    sr = int(file['sr']) 
 
-                # Converting from stereo (2 channels) to mono 
-                array_mono = librosa.core.to_mono(file['array'])
-               
-                # TODO: "Up-sample/down-sample?"
+                    # Converting to mono if 2 channels (all files either 1 or 2)
+                    if file['array'].shape[0] == 2:
+                        array_mono = librosa.core.to_mono(file['array'])
+                    else:
+                        array_mono = file['array']
+                   
+                    # TODO: "Up-sample/down-sample?"
 
-                # Getting log-mel-spectrogram
-                spectrogram = np.log(librosa.feature.melspectrogram(array_mono, sr))
+                    # Getting log-mel-spectrogram, and turning it into a tensor
+                    spectrogram = np.log(librosa.feature.melspectrogram(array_mono, sr))
 
-                tags = get_tags_7digitalid(id)
-                
-                # TODO: Encode+filter tags (after friday when we decide how)
-                # TODO: Output this to where????
+                    tags = q_fm.get_tags(tid) 
+                    
+                    # TODO: Encode+filter tags (after friday when we decide how)
+                    # filter will probably be something like:
+                    # tags = [tag for tag in tags if tag in TAGS]
+                    # We just need a function to get the TAGS.
 
+                    # TODO: Refine following outline of the saving to TFRecords procedure
+                    # TODO: Decide on using Example or SequenceExample
+                    spectrogram_str = tf.io.serialize_tensor(tf.convert_to_tensor(spectrogram))
+                    example = tf.train.Example(
+                            features=tf.train.Features(
+                                feature={
+                                    'spectrogram' : _bytes_feature(spectrogram_str)
+                                    'tid' :         _bytes_feature(bytes(tid)) 
+                                    'tags' :        # TODO: After knowing encoding?
+                            }))
 
-
+                    seq = tf.train.SequenceExample(
+                            context=tf.train.Features(
+                                feature={
+                                    'tid' :         _bytes_feature(tid) 
+                                    'tags' :        # TODO: After knowing encoding?
+                            }),
+                            feature_list=tf.train.FeatureLists(
+                                feature_list={
+                                    'spectrogram' : # TODO: ???
+                            }))
+                    # TODO: Again, SequenceExample vs Example
+                    writer.write(seq.SerializeToString())
+                    writer.write(example.SerializeToString())
