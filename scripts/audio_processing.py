b4230d7caaf3a56554b3cab1708c4c0706442168
Calle Sönne <calle.sonne@gmail.com>
2019-07-18 00:31:31 +0100

    Restructuring

--- scripts/audio_processing.py
+++ scripts/audio_processing.py
@@ -6,6 +6,7 @@
 # Could utilize arguments for this
 
 import os
+import sys
 
 import librosa
 import numpy as np
@@ -18,68 +19,127 @@ from .modules import query_msd_summary as q_msd
 root_dir = '/srv/data/urop/7digital_numpy/'
 TAGS = [] # Allowed tags
 
+
+def get_filepaths():
+    ''' Gets paths to all .npz files and returns them in a list '''
+
+    paths = [] 
+    for i in range(10):
+        for j in range(10):
+            dir = os.path.join(root_dir, str(i), str(j)) # Current directory
+            # Loop through files in directory
+            for file_name in os.listdir(dir):
+                paths.append(os.path.join(dir, file_name))
+    return paths
+
+
+def process_array(array, sr, audio_format):
+    # TODO: Change name of audio format
+    ''' Returns processed array with desired audio format 
+    
+    Summary
+    -------
+    The array is processed by the following steps:
+    1. Converted to mono
+    2. Resampling to 16 kHz
+    3. Array is converted to a desired audio format
+
+    Parameters
+    ----------
+    array : ndarray
+        unprocessed array, directly from the .npz file
+    sr : int
+        sample rate
+    audio_format : str
+        desired audio format
+
+
+    Returns
+    -------
+    ndarray
+        processed array
+    '''
+    
+    # Converting to mono
+    if array.shape[0] == 2:
+        array = librosa.core.to_mono(array)
+    else:
+        array = unprocessed_array
+   
+    # Resampling the file to 16kHz 
+    array = librosa.resample(array, sr, 16000)
+    
+    # Something along these lines?? Very likely to be changed given
+    # how we choose to incorporate sysarg
+    if audio_format == "log-mel-spectrogram"
+        array = np.log(librosa.feature.melspectrogram(array, 16000))
+    elif audio_format == "MFCC":
+        # TODO: Maybe some MFCCs??
+    
+    return array
+
+def get_tid_from_path(path):
+    '''  '''
+
+    # TODO: Get TID using Davides+Adens new database
+    # id_7digital = file_name[:-4]
+    return tid
+
+def filter_tags(tags):
+    '''  '''
+
+    # TODO
+    return tags
+
 def encode_tags(tags):
     ''' Encodes tags for the TFRecords file '''
+
     # TODO
-    return
+    return #???
 
 def _bytes_feature(value):
+    '''  '''
+
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def get_example(array, tid, encoded_tags):
+    '''  '''
+    # TODO: Refine following outline of the saving to TFRecords procedure
+    array_str = tf.io.serialize_tensor(tf.convert_to_tensor(array))
+    example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    'spectrogram' : _bytes_feature(array_str),
+                    'tid' :         _bytes_feature(bytes(tid)),
+                    'tags' :        # TODO: After knowing encoding?
+            }))
+    return example
 
 
 if __name__ == '__main__':
+
+    # TODO: Fix sys args, need to get audio_format 
+
+    paths = get_filepaths()
+        
     with tf.python_io.TFRecordWriter(tf_filename) as writer: # TODO: Decide filename 
-        # Looping through all of the directories containing .npz files
-        for i in range(10):
-            for j in range(10):
+        for path in paths:
+            # Loading the unsampled file from path of npz file   
+            unsampled_file = np.load(path)
+            processed_array = process_array(unsampled_file['array'], 
+                                            unsampled_file['sr'], audio_format)
 
-                dir = os.path.join(root_dir, str(i), str(j)) # Current directory
-                # Loop through files in directory
-                for file_name in os.listdir(dir):
+            # TODO: make get_tid_from_path()
+            tid = get_tid_from_path(path)
+            tags = q_fm.get_tags(tid) 
+            # TODO: make filter_tags() (when we decide how)
+            tags = filter_tags(tags)
+            # TODO: make encode_tags() (when we decide how)
+            encoded_tags = encode_tags(tags) 
 
-                    path = os.path.join(dir, file_name)
-                    
-                    
-                    # TODO: Get TID using Davides+Adens new database
-                    # id_7digital = file_name[:-4]
-                    tid = ""
+            # TODO: Refine get_example() 
+            example = get_example(array, tid, encoded_tags)
+            writer.write(example.SerializeToString())
 
-                    #loading the unsampled file from path of npz file   
-                    unsampled_file = np.load(path)
-                    
-                    #resampling the file to 16kHz 
-                    sampled_file = librosa.resample(unsampled_file["array"], unsampled_file["sr"], 16000)
-
-                    
-
-                    # Converting to mono if 2 channels (all files either 1 or 2)
-                    if sampled_file.shape[0] == 2:
-                        array_mono = librosa.core.to_mono(sampled_file)
-                    else:
-                        array_mono = sampled_file
-                   
-                    # Getting log-mel-spectrogram
-                    # Could probably do have a if arg == "spectrogram" here (maybe also MFCC?)
-                    spectrogram = np.log(librosa.feature.melspectrogram(array_mono, 16000))
-
-                    tags = q_fm.get_tags(tid) 
-                    
-                    # TODO: Encode+filter tags (after friday when we decide how)
-                    # filter will probably be something like:
-                    # tags = [tag for tag in tags if tag in TAGS]
-                    # We just need a function to get the TAGS.
-
-                    # TODO: Refine following outline of the saving to TFRecords procedure
-                    spectrogram_str = tf.io.serialize_tensor(tf.convert_to_tensor(spectrogram))
-                    example = tf.train.Example(
-                            features=tf.train.Features(
-                                feature={
-                                    'spectrogram' : _bytes_feature(spectrogram_str),
-                                    'tid' :         _bytes_feature(bytes(tid)),
-                                    'tags' :        # TODO: After knowing encoding?
-                            }))
-
-                    writer.write(example.SerializeToString())
-
-"these scripts are still fairly untested and should only be used after we sort out the couple of points"
+"These scripts are still fairly untested and should only be used after we sort out the couple of points"
 
