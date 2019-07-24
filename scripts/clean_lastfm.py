68e8daa35eb15a3efa3fc61501f19658c04cb423
Davide Gallo <dvdgallo98@gmail.com>
2019-07-24 19:33:54 +0100

    Rename variable

--- scripts/clean_lastfm.py
+++ scripts/clean_lastfm.py
@@ -16,10 +16,10 @@ def create_tag_tag_table(db: db.LastFm, df: pd.DataFrame):
 
     output = pd.DataFrame(data={'old_lastfm_tag': new_tags}, index=old_tags.rename('new_tag'))
 
-def create_tid_tag_table(db: db.LastFm, df_tag_tag: pd.DataFrame):
+def create_tid_tag_table(db: db.LastFm, df: pd.DataFrame):
     # fetch the tids from the original database, and map the tags to their correspondent 'clean' tags
     col_1 = db.tid_tag['tid']
-    col_2 = db.tid_tag['tag'].apply(lambda t: df_tag_tag['clean_tag'].loc[t])
+    col_2 = db.tid_tag['tag'].apply(lambda t: df['clean_tag'].loc[t])
 
     # concatenate columns in a dataframe
     output = pd.concat([col_1, col_2], axis=1)
