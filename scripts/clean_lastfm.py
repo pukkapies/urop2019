4faa19e6b43a895bdf92ea4dc92c39d56451ad63
Davide Gallo <dvdgallo98@gmail.com>
2019-07-24 18:37:24 +0100

    Add tid_tag_table function

--- scripts/clean_lastfm.py
+++ scripts/clean_lastfm.py
@@ -1,4 +1,18 @@
+import pandas as pd
+
 # from aden import his module
+
+def create_tid_tag_table(db: db.LastFm, df_tag_tag: pd.DataFrame):
+    # fetch the tids from the original database, and map the tags to their correspondent 'clean' tags
+    col_1 = db.tid_tag['tid']
+    col_2 = db.tid_tag['tag'].apply(lambda tag: df_tag_tag['clean_tag'].loc[tag])
+
+    # concatenate columns in a dataframe
+    output = pd.concat([col_1, col_2], axis=1)
+
+    # remove tags which fall below the popularity theshold
+    output = output[output['tag'] != 0]
+    return output
 
 if __name__ = '__main__':
     
