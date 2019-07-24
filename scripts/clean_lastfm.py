0f9df83532ed59a00757ada7b5667148b9368768
Davide Gallo <dvdgallo98@gmail.com>
2019-07-24 19:53:54 +0100

    Use loc instead of get

--- scripts/clean_lastfm.py
+++ scripts/clean_lastfm.py
@@ -24,7 +24,7 @@ def create_tag_tag_table(db: db.LastFm, df: pd.DataFrame):
     new_tags = old_tags.apply(lambda t: flat['old_lastfm_tag'][flat['old_lastfm_tag'] == t].append(pd.Series([0])).index[0])
     
     # for each tag idx, get the corresponding 'clean' tag num from the flattened dataframe (returns tag num = 0 if tag falls below the pop threshold)
-    new_tags = new_tags.apply(lambda i: flat['new_tag'].get(i))
+    new_tags = new_tags.apply(lambda i: flat['new_tag'].loc[i])
 
     output = pd.DataFrame(data={'old_lastfm_tag': new_tags}, index=old_tags.rename('new_tag'))
 
