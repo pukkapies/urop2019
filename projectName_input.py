f8b3c6f14ad7593d7fbd4815a67fcb6424126584
Davide Gallo <dvdgallo98@gmail.com>
2019-08-07 10:27:52 +0100

    Temporarily change feature names

--- projectName_input.py
+++ projectName_input.py
@@ -12,9 +12,9 @@ def set_tfrecords_root_dir(new_root_dir):
     tfrecord_root_dir = new_root_dir
 
 audio_feature_description = {
-    'audio': tf.io.VarLenFeature(tf.float32),
+    'spectogram': tf.io.VarLenFeature(tf.float32),
     'tid': tf.io.VarLenFeature(tf.string),
-    'tag': tf.io.VarLenFeature(tf.int64)
+    'tags': tf.io.VarLenFeature(tf.int64)
 }
 
 def _parse_audio(example):
@@ -25,12 +25,12 @@ def _reshape(data, new_shape):
         new_shape = (new_shape, -1)
 
     return {
-        'audio': tf.sparse.reshape(data['audio'], new_shape),
+        'spectogram': tf.sparse.reshape(data['spectogram'], new_shape),
         'tid': data['tid'],
-        'tag': data['tag']}
+        'tags': data['tags']}
 
 def _tag_filter(data, idxs):
-    tags = tf.unstack(data['tag'])
+    tags = tf.unstack(data['tags'])
     tags = tags.indices.numpy().flatten()
     return np.any(tags == idxs)
 
@@ -42,14 +42,14 @@ def _window(data, s):
     pass
 
 def _batch_normalization(data, epsilon=.0001): # not sure if we need this... there's already a batch normalization layer in the model. not even sure if it works
-    tensor = tf.unstack(data['audio'])
+    tensor = tf.unstack(data['spectogram'])
     mean,variance = tf.nn.moments(tensor, axes=[0])
     tensor_normalized = (tensor-mean)/(variance+epsilon)
     
     return {
-        'audio': tensor_normalized,
+        'spectogram': tensor_normalized,
         'tid': data['tid'],
-        'tag': data['tag']}
+        'tags': data['tags']}
 
 def genrate_dataset(root_dir=tfrecord_root_dir, shuffle=True, batch_size=32, buffer_size=10000, window_size=15, reshape=None, with_tags=None, with_tids=None, num_epochs=None):
     if root_dir:
