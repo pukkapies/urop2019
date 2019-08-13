995938923aa97447785e6d61a9a7454507768725
Chon Wai Ho (Aden) <48314854+hcw1026@users.noreply.github.com>
2019-08-13 07:48:37 +0100

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -18,76 +18,8 @@ import model_keras as Model
 from datetime import datetime
 import os
 import time
+import projectName_input
 
-
-def train_exp(frontend_mode, numOutputNeurons, train_datasets, val_datasets,
-          y_input=None, is_training=True, num_units=1024,
-          num_filt=32, n_epoch=10, lr=0.001):
-    
-    #in case of keyboard interrupt during previous training
-    #tf.summary.trace_off()
-    #initiate tensorboard
-    current_time = datetime.now().strftime('%Y%m%d-%H%M%S')
-    train_log_dir = 'logs/gradient_tape2/' + current_time + '/train'
-    val_log_dir = 'logs/gradient_tape2/' + current_time + '/val'
-    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-    val_summary_writer = tf.summary.create_file_writer(val_log_dir)
-    
-    tf.summary.trace_on(graph=True, profiler=False)
-    
-    #import model
-    model = Model.build_model(frontend_mode=frontend_mode,
-                              numOutputNeurons=numOutputNeurons,
-                              y_input=y_input, is_training=is_training,
-                              num_units=num_units, num_filt=num_filt)
-    
-    
-    #export tensorboard graph
-    with train_summary_writer.as_default():
-        tf.summary.trace_export(
-                name="my_func_trace",
-                step=0)
-        
-    #initialise loss, optimiser, metric
-    loss = tf.keras.losses.MeanSquaredError()
-    optimiser = tf.keras.optimizers.Nadam(learning_rate=lr)
-    
-    #epoch loop
-    for epoch in range(n_epoch):
-        tf.print('Epoch {}'.format(epoch))
-        
-        #initialise metrics per epoch
-        train_AUC = tf.keras.metrics.AUC(name='train_AUC', dtype=tf.float32)
-        val_AUC = tf.keras.metrics.AUC(name='val_AUC', dtype=tf.float32)
-        
-        tf.summary.trace_on(graph=False, profiler=True)
-        #train all batches once
-        for idx, dataset in enumerate(train_datasets):
-            
-            loss_value = train_body(dataset, model, optimiser, loss, train_AUC)
-            print(loss_value)
-        
-        #tensorboard export profiling and record train AUC
-        with train_summary_writer.as_default(): #
-            tf.summary.trace_export(
-                    name="my_func_trace", 
-                    step=0, profiler_outdir=os.path.normpath(train_log_dir)) #
-        
-        with train_summary_writer.as_default():
-            tf.summary.scalar('AUC', train_AUC.result(), step=epoch)
-            
-        #reset train metric per epoch
-        train_AUC.reset_states()
-        
-        #run validation over all batches
-        for dataset in val_datasets:
-            val_body(dataset, model, val_AUC)
-        
-        with val_summary_writer.as_default():
-            tf.summary.scalar('AUC', val_AUC.result(), step=epoch)
-        
-        #reset val metric per epoch
-        val_AUC.reset_states()  
 
 
 @tf.function
@@ -108,8 +40,6 @@ def train_comp(model, optimiser, x_batch_train, y_batch_train, loss):
 def transformation(x_batch_train, y_batch_train):
 
     x_batch_train = tf.sparse.to_dense(x_batch_train)
-    x_batch_train = tf.expand_dims(x_batch_train, 0)
-    y_batch_train = tf.reshape(y_batch_train, (-1, 155))
     return x_batch_train, y_batch_train
     
     
@@ -120,13 +50,17 @@ def train_body(dataset, model, optimiser, loss, train_AUC):
     loss_value=0.
         
     for step, entry in dataset.enumerate():
-        x_batch_train, y_batch_train, tid = entry['audio'], entry['tags'], entry['tid']
+        x_batch_train, y_batch_train = entry['audio'], entry['tags']
         x_batch_train, y_batch_train = transformation(x_batch_train, y_batch_train)
                 ##!!!currently incompatible with testing.py
                 
         loss_value, logits = train_comp(model, optimiser, x_batch_train, y_batch_train, loss)
             
         train_AUC(y_batch_train, logits)
+        
+        if tf.equal(step%100, 0):   #change here for printing frequency
+            tf.print('Step', step, ': loss', loss_value, '; AUC', train_AUC.result())
+            
             
     return loss_value
             
@@ -137,7 +71,7 @@ def val_body(dataset, model, val_AUC):
     tf.keras.backend.set_learning_phase(0)
     
     for entry in dataset:
-        x_batch_val, y_batch_val, tid = entry['audio'], entry['tags'], entry['tid']
+        x_batch_val, y_batch_val = entry['audio'], entry['tags']
         x_batch_val, y_batch_val = transformation(x_batch_val, y_batch_val)
                 ##!!!currently incompatible with testing.py
             
@@ -147,105 +81,88 @@ def val_body(dataset, model, val_AUC):
     
     #set training phase =1
     tf.keras.backend.set_learning_phase(1)
-            
-       
-
-#####################################################################################
 
 
-def train(frontend_mode, numOutputNeurons, train_datasets, val_datasets,
-          y_input=None, is_training=True, num_units=1024,
-          num_filt=32, n_epoch=10, lr=0.001):
+
+def train(frontend_mode, train_datasets, val_datasets, numOutputNeurons=155,
+          y_input=96, num_units=1024, num_filt=32, n_epoch=10, lr=0.001,
+          log_dir = 'logs/trial1/'):
     
+    #in case of keyboard interrupt during previous training
+    tf.summary.trace_off()
+    
+    #initiate tensorboard
     current_time = datetime.now().strftime('%Y%m%d-%H%M%S')
-    train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
-    val_log_dir = 'logs/gradient_tape/' + current_time + '/val'
+    train_log_dir = log_dir + current_time + '/train'
+    val_log_dir = log_dir + current_time + '/val'
+    prof_log_dir = log_dir + current_time + '/prof'
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
     val_summary_writer = tf.summary.create_file_writer(val_log_dir)
- 
-    tf.summary.trace_on(graph=True, profiler=False)
+    prof_summary_writer = tf.summary.create_file_writer(prof_log_dir)
+    
+    #import model
     model = Model.build_model(frontend_mode=frontend_mode,
                               numOutputNeurons=numOutputNeurons,
-                              y_input=y_input, is_training=is_training,
-                              num_units=num_units, num_filt=num_filt)
-    
-    with train_summary_writer.as_default():
-        tf.summary.trace_export(
-                name="my_func_trace",
-                step=0)
-    
+                              y_input=y_input, num_units=num_units, 
+                              num_filt=num_filt)
+        
+    #initialise loss, optimiser, metric
     loss = tf.keras.losses.MeanSquaredError()
     optimiser = tf.keras.optimizers.Nadam(learning_rate=lr)
     
-    
-    #delete later:
-    x_train, y_train = exp_spec()
-    x_train = tf.constant(x_train, dtype=tf.float32)
-    y_train = tf.constant(y_train, dtype=tf.float32)
-    
+    #epoch loop
     for epoch in range(n_epoch):
-        print('Epoch {}'.format(epoch))
+        tf.print('Epoch {}'.format(epoch))
         
-         #https://www.tensorflow.org/tensorboard/r2/get_started
+        #initialise metrics per epoch
         train_AUC = tf.keras.metrics.AUC(name='train_AUC', dtype=tf.float32)
         val_AUC = tf.keras.metrics.AUC(name='val_AUC', dtype=tf.float32)
-        tf.summary.trace_on(graph=False, profiler=True)##
+        
+        tf.summary.trace_on(graph=False, profiler=True)
+        #train all batches once
         for idx, dataset in enumerate(train_datasets):
-           
-            for step, entry in enumerate(dataset):
-                x_batch_train, y_batch_train, tid = entry['audio'], entry['tags'], entry['tid']
-                x_batch_train = tf.sparse.to_dense(x_batch_train)
-                x_batch_train = tf.expand_dims(x_batch_train, 0)
-                y_batch_train = tf.reshape(y_batch_train, (1, 155))
-                ##!!!currently incompatible with testing.py
-                
-                start = time.time()
-                with tf.GradientTape() as tape:
-                    #tf.summary.trace_on(graph=False, profiler=True)
-                    logits = model(x_batch_train)
-                    #with train_summary_writer.as_default():
-                    #    tf.summary.trace_export(
-                    #            name="my_func_trace",
-                    #            step=0, profiler_outdir=os.path.normpath(train_log_dir))
-                    loss_value = loss(y_batch_train, logits)
+            tf.print('tfrecord {}'.format(idx))
+            
+            loss_value = train_body(dataset, model, optimiser, loss, train_AUC)
+            
+            #print progress
+            tf.print('tfrecord {} done'.format(idx))
+            tf.print('Epoch', epoch, ': tfrecord', idx, '; loss', loss_value, '; AUC', train_AUC.result())
+            tf.print(optimiser.iterations)
+            tf.print(train_AUC.result())
+            #log to tensorboard
+            with train_summary_writer.as_default():
+                tf.summary.scalar('AUC', train_AUC.result(), step=int(optimiser.iterations.numpy()))
+                tf.summary.scalar('Loss', loss_value, step=int(optimiser.iterations.numpy()))
+            
+        #print progress
+        tf.print('Epoch {} --training done'.format(epoch))
+        tf.print('Epoch', epoch, ': loss', loss_value, '; AUC', train_AUC.result())
         
-                grads = tape.gradient(loss_value, model.trainable_weights)
-                optimiser.apply_gradients(zip(grads, model.trainable_weights))
-        
-                train_AUC(y_batch_train, logits)
-                duration = time.time()-start
-                print(duration)
-        with train_summary_writer.as_default(): #
+        #tensorboard export profiling and record train AUC and loss
+       
+        with prof_summary_writer.as_default():   
             tf.summary.trace_export(
-                    name="my_func_trace", 
-                    step=0, profiler_outdir=os.path.normpath(train_log_dir)) #
-
-        
-        with train_summary_writer.as_default():
-            tf.summary.scalar('AUC', train_AUC.result(), step=epoch)
+                    name="trace", 
+                    step=epoch, profiler_outdir=os.path.normpath(prof_log_dir)) 
             
-        print(loss_value.numpy())
-            
+        #reset train metric per epoch
         train_AUC.reset_states()
         
-        for val_dataset in val_datasets:
-            
-            for entry in val_dataset:
-                x_batch_val, y_batch_val, tid = entry['audio'], entry['tags'], entry['tid']
-                x_batch_val = tf.sparse.to_dense(x_batch_val)
-                x_batch_val = tf.expand_dims(x_batch_val, 0)
-                y_batch_val = tf.reshape(y_batch_val, (1, 155))
-                ##!!!currently incompatible with testing.py
-            
-                val_logits = model(x_batch_val)
-                #calculate AUC
-                val_AUC(y_batch_val, val_logits)
-            
+        #run validation over all batches
+        for dataset in val_datasets:
+            val_body(dataset, model, val_AUC)
+        
         with val_summary_writer.as_default():
             tf.summary.scalar('AUC', val_AUC.result(), step=epoch)
-            
-        val_AUC.reset_states()
         
+        #reset val metric per epoch
+        val_AUC.reset_states()  
+        
+def main():
+    return
+
+#####################################################################################
             
     
     
