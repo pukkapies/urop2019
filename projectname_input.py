189a101a80d984e37f8504468fdf0f3cf47cd041
Davide Gallo <dvdgallo98@gmail.com>
2019-08-15 13:12:54 +0100

    Add _tag_merge

--- projectname_input.py
+++ projectname_input.py
@@ -23,11 +23,14 @@ Functions
 - _reshape
     Reshape flattened audio tensors into the original ones.
 
+- _tag_merge
+    Merge similar tags together.
+
+- _tag_filter
+    Remove (tracks with) unwanted tags from the dataset.
+
 - _tid_filter
     Remove (tracks with) unwanted tids from the dataset.
-
-- _tad_filter
-    Remove (tracks with) unwanted tags from the dataset.
 
 - _tag_filter_hotenc_mask
     Change the shape of tag hot-encoded vector to suit the output of _tag_filter.
@@ -68,19 +71,42 @@ def _reshape(features_dict, shape):
     features_dict['audio'] = tf.sparse.reshape(features_dict['audio'], shape)
     return features_dict
 
-def _tid_filter(features_dict, tids):
-    ''' Removes unwanted tids from the dataset (use with tf.data.Dataset.filter).
-        
+def _tag_merge(features_dict, merge_tags):
+    ''' Removes unwanted tids from the dataset (use with tf.data.Dataset.map).
+    
     Parameters
     ----------
     features_dict : dict
-        Dict of features (as provided by .filter).
+        Dict of features (as provided by .map).
 
-    tids : list or list-like
-        List containing tids (as strings) to be "allowed" in the output dataset.
+    merge_tags : list or list-like
+        List of lists of tags to be merged. Writes 1 for all tags in the hot-encoded vector whenever at least one tag of the list is present.
+
+    Examples
+    --------
+    >>> features['tags'] = [0, 1, 1, 0, 0]
+    >>> _tag_merge(features, merge_tags=[[0, 1], [2, 3]])
+    features['tags']: [1, 1, 1, 1, 0]
+    >>> _tag_merge(features, merge_tags=[[0, 1], [3, 4]])
+    features['tags']: [1, 1, 1, 0, 0] 
     '''
+    
+    merge_tags = np.array(merge_tags)
 
-    return tf.math.reduce_any(tf.math.equal(tids, features_dict['tid']))
+    assert len(merge_tags.shape) == 2 # sanity check
+
+    for tags in merge_tags: # for each list of tags in 'merge_tags' (which is a list of lists...)
+        tags = tf.SparseTensor(indices=np.subtract(np.array(tags, dtype=np.int64).reshape(-1, 1), 1), values=np.ones(len(tags), dtype=np.int64), dense_shape=np.array([N_TAGS], dtype=np.int64))
+        tags = tf.sparse.to_dense(tags)
+        tags = tf.dtypes.cast(tags, tf.bool)
+
+        feature_tags = tf.dtypes.cast(features_dict['tags'], tf.bool)
+
+        # if at least one of the feature tags is in the current 'tags' list, write True in the bool-hot-encoded vector for all tags in 'tags'; otherwise, leave feature tags as they are
+        features_dict['tags'] = tf.where(tf.math.reduce_any(tags & feature_tags), tags | feature_tags, feature_tags)
+        features_dict['tags'] = tf.cast(features_dict['tags'], tf.float32) # cast back to float32
+        
+    return features_dict
 
 def _tag_filter(features_dict, tags):
     ''' Removes unwanted tids from the dataset (use with tf.data.Dataset.filter).
@@ -101,6 +127,20 @@ def _tag_filter(features_dict, tags):
     tags_mask = tf.dtypes.cast(tags_mask, tf.bool)
 
     return tf.math.reduce_any(feature_tags & tags_mask) # returns True if and only if at least one feature tag is in the desired 'tags' list
+
+def _tid_filter(features_dict, tids):
+    ''' Removes unwanted tids from the dataset (use with tf.data.Dataset.filter).
+        
+    Parameters
+    ----------
+    features_dict : dict
+        Dict of features (as provided by .filter).
+
+    tids : list or list-like
+        List containing tids (as strings) to be "allowed" in the output dataset.
+    '''
+
+    return tf.math.reduce_any(tf.math.equal(tids, features_dict['tid']))
 
 def _tag_filter_hotenc_mask(features_dict, tags):
     ''' Reshapes tag hot-encoded vector after filtering with _tag_filter (use with tf.data.Dataset.map).
@@ -185,7 +225,7 @@ def _batch_tuplification(features_dict):
 
     return (features_dict['audio'], features_dict['tags'])
 
-def generate_dataset(tfrecords, audio_format, batch_size=32, shuffle=True, buffer_size=10000, window_length=15, random=False, with_tags=None, with_tids=None, num_epochs=None, as_tuple=True):
+def generate_dataset(tfrecords, audio_format, batch_size=32, shuffle=True, buffer_size=10000, window_length=15, random=False, with_tids=None, with_tags=None, merge_tags=None, num_epochs=None, as_tuple=True):
     ''' Reads the TFRecords and produce a tf.data.Dataset ready to be iterated during training/evaluation.
     
     Parameters:
@@ -211,11 +251,14 @@ def generate_dataset(tfrecords, audio_format, batch_size=32, shuffle=True, buffe
     random : bool
         Specifies how the window is to be extracted. If True, slices the window randomly (default is pick from the middle).
 
+    with_tids : list
+        If not None, contains the tids to be trained on.
+
     with_tags : list
         If not None, contains the tags to be trained on.
 
-    with_tids : list
-        If not None, contains the tids to be trained on.
+    merge_tags : list
+        If not None, contains the lists of tags to be merged together (only applies if with_tags is specified).
 
     num_epochs : int
         If not None, repeats the dataset only for a given number of epochs (default is repeat indefinitely).
@@ -253,7 +296,11 @@ def generate_dataset(tfrecords, audio_format, batch_size=32, shuffle=True, buffe
 
     # apply tid and tag filters
     if with_tags:
-        dataset = dataset.filter(lambda x: _tag_filter(x, with_tags)).map(lambda x: _tag_filter_hotenc_mask(x, with_tags))
+        if merge_tags is not None:
+            dataset = dataset.map(lambda x: _tag_merge(x, merge_tags))
+            dataset = dataset.filter(lambda x: _tag_filter(x, with_tags)).map(lambda x: _tag_filter_hotenc_mask(x, with_tags))
+        else:
+            dataset = dataset.filter(lambda x: _tag_filter(x, with_tags)).map(lambda x: _tag_filter_hotenc_mask(x, with_tags))
     if with_tids:
         dataset = dataset.filter(lambda x: _tid_filter(x, with_tids))
     
