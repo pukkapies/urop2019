a8f8c9c7c49d8a3684b6867f5fb4b38c56fff4ac
Davide Gallo <dvdgallo98@gmail.com>
2019-08-28 20:45:50 +0200

    General improvements

--- training.py
+++ training.py
@@ -18,15 +18,15 @@ def _required_length(nmin, nmax):
                 setattr(args, self.dest, values)
         return RequiredLength
 
-def parse_config(path_config, path_lastfm):
+def parse_config(config_path, lastfm_path):
 
     # load tags database
-    lastfm = LastFm(os.path.expanduser(path_lastfm))
+    lastfm = LastFm(os.path.expanduser(lastfm_path))
 
-    if not os.path.isfile(os.path.expanduser(path_config)):
-        path = os.path.join(os.path.abspath(os.path.expanduser(path_config)), 'config.json')
+    if not os.path.isfile(os.path.expanduser(config_path)):
+        path = os.path.join(os.path.abspath(os.path.expanduser(config_path)), 'config.json')
     else:
-        path = os.path.expanduser(path_config)
+        path = os.path.expanduser(config_path)
 
     # load json
     with open(path, 'r') as f:
@@ -49,21 +49,24 @@ def parse_config(path_config, path_lastfm):
     config.cycle_len = config_d['config']['cycle_length']
     config.early_stop_min_d = config_d['config']['early_stop_min_delta']
     config.early_stop_patience = config_d['config']['early_stop_patience']
-    config.log = config_d['config']['log_dir']
     config.n_dense_units = config_d['model']['n_dense_units']
     config.n_filters = config_d['model']['n_filters']
     config.n_mels = config_d['tfrecords']['n_mels']
     config.n_output_neurons = len(tags)
+    config.path = config_path
     config.plateau_min_d = config_d['config']['reduce_lr_plateau_min_delta']
     config.plateau_patience = config_d['config']['reduce_lr_plateau_patience']
     config.shuffle = config_d['config']['shuffle']
     config.shuffle_buffer = config_d['config']['shuffle_buffer_size']
+    config.split = config_d['config']['split']
     config.sr = config_d['tfrecords']['sample_rate']
     config.tags = lastfm.vec_tag_to_tag_num(list(tags))
     config.tags_to_merge = lastfm.tag_to_tag_num(config_d['tags']['merge']) if config_d['tags']['merge'] is not None else None
     config.tot_tags = config_d['tfrecords']['n_tags']
     config.window_len = config_d['config']['window_length']
     config.window_random = config_d['config']['window_extract_randomly']
+    config.log_dir: config_d['config']['log_dir']
+    config.checkpoint_dir: config_d['config']['checkpoint_dir']
 
     # create config namespace for the optimizer (will be used by get_optimizer() in order to allow max flexibility)
     config_optim = argparse.Namespace()
@@ -72,7 +75,7 @@ def parse_config(path_config, path_lastfm):
     
     return config, config_optim
 
-def get_compiled_model(frontend, strategy, config, config_optim, checkpoint_path=None):
+def get_compiled_model(frontend, strategy, config, config_optim, resume_time=None):
     with strategy.scope():
         # read optimizer specs from config_optim dict for max flexibility
         optimizer = tf.keras.optimizers.get({"class_name": config_optim.class_name, "config": config_optim.config})
@@ -81,14 +84,14 @@ def get_compiled_model(frontend, strategy, config, config_optim, checkpoint_path
         model = projectname.build_model(frontend, num_output_neurons=config.n_output_neurons, num_units=config.n_dense_units, num_filts=config.n_filters, y_input=config.n_mels)
         model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy(from_logits=False, reduction=tf.keras.losses.Reduction.SUM), metrics=[[tf.keras.metrics.AUC(curve='ROC', name='AUC-ROC'), tf.keras.metrics.AUC(curve='PR', name='AUC-PR')]])
         
-        # restore checkpoint_path (if provided)
-        if checkpoint_path:
-            model.load_weights(os.path.expanduser(checkpoint_path))
+        # restore resume_time (if provided)
+        if resume_time:
+            model.load_weights(os.path.expanduser(resume_time))
     return model
 
-def train(train_dataset, valid_dataset, frontend, strategy, config, config_optim, epochs, steps_per_epoch=None, checkpoint_path=None, update_freq=1, profile_batch=0):
+def train(train_dataset, valid_dataset, frontend, strategy, config, config_optim, epochs, steps_per_epoch=None, resume_time=None, update_freq=1, profile_batch=0):
 
-    model = get_compiled_model(frontend, strategy, config, config_optim, checkpoint_path)
+    model = get_compiled_model(frontend, strategy, config, config_optim, resume_time)
 
     log_dir = os.path.join(os.path.expanduser(config.log), frontend, datetime.datetime.now().strftime("%y%m%d-%H%M")) # to access training scalars using tensorboard
 
@@ -140,15 +143,13 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument("frontend", choices=["waveform", "log-mel-spectrogram"])
-    parser.add_argument("-a", "--api", choices=["fit", "custom"], default="fit")
-    parser.add_argument("-s", "--split", help="percentage of tracks to go in each dataset, supply as TRAIN VAL (TEST)", type=int, nargs='+', action=_required_length(2,3))
     parser.add_argument("--root-dir", dest="tfrecords_dir", help="directory to read .tfrecord files from (default to path on Boden)")
     parser.add_argument("--path-config", help="path to config.json (default to path on Boden)", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json'))
     parser.add_argument("--path-lastfm", help="path to (clean) lastfm database (default to path on Boden)", default="/srv/data/urop/clean_lastfm.db")
     parser.add_argument("--epochs", help="specify the number of epochs to train on", type=int, required=True)
     parser.add_argument("--steps-per-epoch", help="specify the number of steps to perform for each epoch (if unspecified, go through the whole dataset)", type=int)
-    parser.add_argument("--checkpoint_path", help="load a previously saved model")
-    parser.add_argument("--update_freq", help="specify the frequency (in steps) to record metrics and losses", type=int, default=10)
+    parser.add_argument("--resume-time", help="load a previously saved model")
+    parser.add_argument("--update-freq", help="specify the frequency (in steps) to record metrics and losses", type=int, default=10)
     parser.add_argument("--cuda", help="set cuda visible devices", type=int, nargs="+")
     parser.add_argument("-v", "--verbose", choices=['0', '1', '2', '3'], help="verbose mode", default='2')
 
@@ -174,6 +175,9 @@ if __name__ == '__main__':
         args.tfrecords_dir = os.path.normpath("/srv/data/urop/tfrecords-" + args.frontend + s)
 
     # create training and validation datasets
+    assert config.split
+    assert len(config.split) >= 2
+    assert len(config.split) <= 3
     train_dataset, valid_dataset = projectname_input.generate_datasets_from_dir(args.tfrecords_dir, args.frontend, split=args.split, which_split=(True, True, ) + (False, ) * (len(args.split)-2),
                                                                                 sample_rate=config.sr, batch_size=config.batch, 
                                                                                 cycle_length=config.cycle_len, 
@@ -187,5 +191,5 @@ if __name__ == '__main__':
     # train
     train(train_dataset, valid_dataset, frontend=args.frontend, strategy=strategy,
           config=config, config_optim=config_optim,
-          epochs=args.epochs, steps_per_epoch=args.steps_per_epoch, checkpoint_path=args.checkpoint_path, 
+          epochs=args.epochs, steps_per_epoch=args.steps_per_epoch, resume_time=args.resume_time, 
           update_freq=args.update_freq)
