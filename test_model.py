f411a3b83f703402db1b4ad935ddaa86f19bb7f1
callesonne <calle.sonne@gmail.com>
2019-08-29 14:59:32 +0100

    bugfixing

--- test_model.py
+++ test_model.py
@@ -8,7 +8,7 @@ import modules.query_lastfm as q_fm
 import projectname_input
 import projectname as Model
 
-def load_from_checkpoint(checkpoint_dir, audio_format, config_dir):
+def load_from_checkpoint(checkpoint_path, audio_format, config_dir):
     ''' Loads model from checkpoint '''
 
     # getting config settings
@@ -18,21 +18,27 @@ def load_from_checkpoint(checkpoint_dir, audio_format, config_dir):
     with open(config_dir) as f:
         file = json.load(f)
 
-    num_tags = file['dataset_specs']['n_tags']
     y_input = file['dataset_specs']['n_mels']
     num_units = file['training_options']['n_dense_units']
     num_filt = file['training_options']['n_filters']
+    num_output_neurons = file['training_options']['n_output_neurons']
 
     # loading model
     model = Model.build_model(frontend_mode=audio_format, 
-                                num_output_neurons=num_tags, y_input=y_input,
+                                num_output_neurons=num_output_neurons, y_input=y_input,
                                 num_units=num_units, num_filt=num_filt)
 
-    # loading latest training checkpoint 
     checkpoint = tf.train.Checkpoint(model=model)
-    latest = tf.train.latest_checkpoint(checkpoint_dir)
-    print('Loadind from {}'.format(latest))
-    checkpoint.restore(latest)
+    latest = tf.train.latest_checkpoint(checkpoint_path)
+    if latest:
+        # loading latest training checkpoint 
+        print('Loading from {}'.format(latest))
+        checkpoint.restore(latest)
+    else:
+        # assuming path was a specific checkpoint
+        print('Loading from {}'.format(checkpoint_path))
+        checkpoint.restore(checkpoint_path)
+
     return model
 
 def test(model, tfrecords_dir, audio_format, split, batch_size=64, window_size=15, random=False, with_tags=None, with_tids=None, merge_tags=None):
@@ -84,12 +90,13 @@ def predict(model, audio, audio_format, with_tags, sample_rate, cutoff=0.5, wind
     slices = get_slices(audio, audio_format, sample_rate, window_size)
     logits = tf.reduce_mean(model(slices, training=False), axis=[0])
     
-    # get tags
-    tags = []
-    for idx, val in enumerate(logits):
-        if val >= cutoff:
-            tags.append((fm.tag_num_to_tag(with_tags[idx]), val.numpy()))
-    return tags
+    return logits
+    # # get tags
+    # tags = []
+    # for idx, val in enumerate(logits):
+    #     if val >= cutoff:
+    #         tags.append((fm.tag_num_to_tag(with_tags[idx]+1), val.numpy()))
+    # return tags
 
 if __name__ == '__main__':
     # getting tags
@@ -97,7 +104,7 @@ if __name__ == '__main__':
     tags = fm.popularity().tag.to_list()[:50]
     with_tags = [fm.tag_to_tag_num(tag) for tag in tags]
 
-    model = load_from_checkpoint('/srv/data/urop/model_aden/log-mel-spectrogram_20190826-103644/', 'log-mel-spectrogram', '/home/calle') 
+    model = load_from_checkpoint('/srv/data/urop/model/log-mel-spectrogram_20190826-103644/epoch-18', 'log-mel-spectrogram', '/home/calle') 
     # loading model
     # test(model, '/srv/data/urop/tfrecords-log-mel-spectrogram/', 'log-mel-spectrogram', (80, 10, 10), batch_size=128, with_tags=with_tags)
 
@@ -110,12 +117,14 @@ if __name__ == '__main__':
 
     dataset = tf.data.TFRecordDataset('/srv/data/urop/tfrecords-log-mel-spectrogram/log-mel-spectrogram_1.tfrecord')
     dataset = dataset.map(lambda x: projectname_input._parse_features(x, AUDIO_FEATURES_DESCRIPTION, (96, -1)))
+    dataset = dataset.filter(lambda x: projectname_input._tag_filter(x, with_tags)).map(lambda y: projectname_input._tag_filter_hotenc_mask(y, with_tags))
     i = 0 
-    for entry in dataset.take(3):
-        if i <= 1:
-            i += 1
+    for entry in dataset.take(5):
+        if i < 4:
+            i +=1
             continue
         tid = entry['tid'].numpy().decode('utf-8')
         print('TID: ', tid)
         print('tags: ', [tag for tag in fm.query_tags(tid) if tag in tags])
-        print('predicted tags: ', predict(model, entry['audio'], 'log-mel-spectrogram', with_tags, 16000, cutoff=0.0001))
+        print('tfrecord tags: ', entry['tags'])
+        print('predicted tags: ', predict(model, entry['audio'], 'log-mel-spectrogram', with_tags, 16000, cutoff=0.1))
