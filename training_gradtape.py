d612f9166b48782b66a1132da56ceb4fabca45cc
Davide Gallo <dvdgallo98@gmail.com>
2019-09-03 10:45:08 +0200

    Remove _required_length

--- training_gradtape.py
+++ training_gradtape.py
@@ -52,15 +52,6 @@ import projectname
 import projectname_input
 from modules.query_lastfm import LastFm
 
-def _required_length(nmin, nmax):
-        class RequiredLength(argparse.Action):
-            def __call__(self, parser, args, values, option_string=None):
-                if not nmin<=len(values)<=nmax:
-                    msg='argument "{f}" requires between {nmin} and {nmax} arguments'.format(f=self.dest, nmin=nmin, nmax=nmax)
-                    raise argparse.ArgumentTypeError(msg)
-                setattr(args, self.dest, values)
-        return RequiredLength
-
 def parse_config(config_path, lastfm_path):
 
     # load tags database
