8f20fcc63edfb7c9fb7395bb89b58b41105651eb
Davide Gallo <dvdgallo98@gmail.com>
2019-09-18 08:38:40 +0100

    Add experimental functions

--- code/msd/lastfm_cleaning.py
+++ code/msd/lastfm_cleaning.py
@@ -9,8 +9,8 @@ page should contain all the options you might possibly need.
 The script relies on the query_lastfm module to perform queries on the original database, and on the
 lastfm_tool module to obatain a dataframe with the tags to retain and the tags to merge.
 
-The script will output a db file similar in structure to the original lastfm_tags.db. The query_lastfm
-module will work on the new db file as well.
+The script will output a .db file similar in structure to the original lastfm_tags.db. The query_lastfm
+module will work on the new .db file as well.
 
 
 Functions
@@ -26,6 +26,15 @@ Functions
 
 - create_tid_tag_table
     Create a dataframe containing all the 'clean' tags for each tid.
+
+- experimental_clean_add
+    Perform advanced cleaning operations on the tid_tag dataframe, and add new tags to it depending on correlation between tags.
+
+- experimental_clean_remove1
+    Perform advanced cleaning operations on the tid_tag dataframe, and remove some tags from it depending on correlation between tags.
+
+- experimental_clean_remove2
+    Perform advanced cleaning operations on the tid_tag dataframe, and remove some tags from it depending on correlation between tags.
 '''
 
 import argparse
@@ -35,13 +44,14 @@ import sys
 
 import pandas as pd
 
+import lastfm_cleaning_utils as lastfm_utils
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')))
 
 from lastfm import LastFm
 from lastfm import LastFm2Pandas
-from lastfm import Matrix
 
-def flatten(df: pd.DataFrame):
+def flatten(df):
     ''' Produce a dataframe with the following columns: 'tag', 'new_tag_num'.
 
     Parameters
@@ -51,24 +61,11 @@ def flatten(df: pd.DataFrame):
 
     Examples
     --------
-    If the original df looks like...
-
-        |    tags        |  merge_tags
-    ---------------------------------------------
-    1   |    rock        |  ['ROCK']
-    2   |    pop         |  []
-    3   |    hip-hop     |  ['hiphop', 'hip hop']
-
-                                ...then flatten(df) returns:
-
-        |    tag         |  tag_num
-    ---------------------------------------------
-    1   |   rock         |  1
-    2   |   ROCK         |  1
-    3   |   pop          |  2
-    4   |   hip hop      |  3
-    5   |   hiphop       |  3
-    6   |   hip-hop      |  3
+    If one entry has tag 'hip-hop' and merge_tags ['hiphop', 'hip hop'], will unpack the tags
+    in merge_tags to have one single merge_tag for each row. 
+    If such original entry has index 3, will produce the following 4 different 'tag'/'tag_num' entries: 'hip hop' / 3, 'hiphop' / 3, 
+    'hiphop' / 3 and 'hip-hop' / 3.
+    In other words, the orginal dataframe will be 'flattened out'.
     '''
 
     tags = [] 
@@ -83,12 +80,12 @@ def flatten(df: pd.DataFrame):
     output = pd.DataFrame(data={'tag': tags, 'new_tag_num': tags_nums})
     return output
 
-def flatten_to_tag_num(lf: LastFm, df: pd.DataFrame):
+def flatten_to_tag_num(fm, df):
     ''' Produce a dataframe with the following columns: 'tag_num', 'new_tag_num'. The tags in the tag column in flatten() are substituted by their original tag nums.
     
     Parameters
     ----------
-    lf: LastFm, LastFm2Pandas
+    fm: LastFm, LastFm2Pandas
         Any instance of the tags database.
 
     df: pd.DataFrame
@@ -96,17 +93,17 @@ def flatten_to_tag_num(lf: LastFm, df: pd.DataFrame):
     '''
 
     output = flatten(df)
-    output['tag'] = output['tag'].map(lf.tag_to_tag_num)
+    output['tag'] = output['tag'].map(fm.tag_to_tag_num)
     output.columns = ['tag_num', 'new_tag_num']
     return output
 
-def create_tag_tag_table(lf: LastFm, df: pd.DataFrame):
+def create_tag_tag_table(fm, df):
     '''
     Produce a series having the old 'tag_num' as index, as the 'new_tag_num' as values. This will contain all the tags from the original tags database, and 0 as a new tag if the old tag has been discarded.
     
     Parameters
     ----------
-    lf: LastFm, LastFm2Pandas
+    fm: LastFm, LastFm2Pandas
         Any instance of the tags database.
 
     df: pd.DataFrame
@@ -121,25 +118,25 @@ def create_tag_tag_table(lf: LastFm, df: pd.DataFrame):
                 return 0
         return fn
 
-    flat = flatten_to_tag_num(lf, df)
+    flat = flatten_to_tag_num(fm, df)
     flat = flat.set_index('tag_num', verify_integrity=True).sort_index()
     
     # define a new 'loc_except' function that locates an entry if it exists, and returns 0 otherwise
     loc_except = locate_with_except(flat['new_tag_num'])
 
     # for each tag num, get the corresponding 'clean' tag num from the flattened dataframe (returns tag num = 0 if tag falls below the pop threshold)
-    tag_tag = pd.Series(lf.get_tag_nums())
+    tag_tag = pd.Series(fm.get_tag_nums())
     tag_tag.index = tag_tag.values
     tag_tag = tag_tag.map(loc_except)
     return tag_tag
 
-def create_tid_tag_table(lf: LastFm, tag_tag: pd.DataFrame, tid_tag_threshold: int = None):
+def create_tid_tag_table(fm, tag_tag, tid_tag_threshold = None):
     '''
     Produce a dataframe with the following columns: 'tid', 'tag'. 
 
     Parameters
     ----------
-    lf: LastFm, LastFm2Pandas
+    fm: LastFm, LastFm2Pandas
         Any instance of the tags database.
 
     tag_tag: pd.DataFrame
@@ -151,9 +148,9 @@ def create_tid_tag_table(lf: LastFm, tag_tag: pd.DataFrame, tid_tag_threshold: i
     
     # fetch the tids from the original database, and map the tags to their correspondent 'clean' tags
     if tid_tag_threshold is not None:
-        tid_tag = lf.fetch_all_tids_tags_threshold(tid_tag_threshold)
+        tid_tag = fm.fetch_all_tids_tags_threshold(tid_tag_threshold)
     else:
-        tid_tag = lf.fetch_all_tids_tags()
+        tid_tag = fm.fetch_all_tids_tags()
     
     col_1 = tid_tag['tid']
     col_2 = tid_tag['tag'].map(tag_tag)
@@ -165,63 +162,191 @@ def create_tid_tag_table(lf: LastFm, tag_tag: pd.DataFrame, tid_tag_threshold: i
     tid_tag = tid_tag[tid_tag['tag'] != 0]
     return tid_tag
 
-if __name__ == '__main__':
+def experimental_clean_add(tid_tag, matrix):
+    '''
+    If the correlation between tag-A and tag-B is higher than a certain threshold (which will determine
+    how 'matrix' is generated), add tag-B to all the tracks which have tag-A but not tag-B.
 
-    import lastfm_cleaning_utils as lastfm_utils
+    Parameters
+    ----------
+    tid_tag: pd.DataFrame
+        The tag_tag dataframe produced with create_tid_tag_table().
+    
+    matrix: np.ndarray
+        The two-dimensional correlation matrix produced with lastfm.Matrix.all_tag_is().
+
+    Examples
+    --------
+    If 'hard rock' is 80% correlated with 'rock', and threshold is less than 80%, add 'rock' to that 20% which has 'hard rock' but not 'rock'.
+    '''
+
+    for tag_x, tag_y in zip(*np.where(matrix>0)):
+        have_tag_x = tid_tag[tid_tag['tag'] == tag_x+1]
+        have_tag_y = tid_tag[tid_tag['tag'] == tag_y+1]
+        
+        have_both = have_tag_x.reset_index().merge(have_tag_y.reset_index(), on='tid', how='inner')
+        
+        to_add = have_tag_x.drop(have_both.index_x, inplace=False) # tracks with tag_x, but without tag_y
+        to_add['tag'] = tag_y + 1
+        
+        tid_tag = tid_tag.append(to_add, ignore_index=True, sort=True)
+    return tid_tag
+
+def experimental_clean_remove1(tid_tag, matrix):
+    '''
+    If the correlation between tag-A and tag-B is higher than a certain threshold (which will determine
+    how 'matrix' is generated), remove tag-B from the tracks which have both tag-A and tag-B.
+
+    Parameters
+    ----------
+    tid_tag: pd.DataFrame
+        The tag_tag dataframe produced with create_tid_tag_table().
+    
+    matrix: np.ndarray
+        The two-dimensional correlation matrix produced with lastfm.Matrix.all_tag_is().
+
+    Examples
+    --------
+    If 'hard rock' is 80% correlated with 'rock', and threshold is less than 80%, take that 80% and remove the tag 'rock'.
+    '''
+
+    for tag_x, tag_y in zip(*np.where(matrix>0)):
+        have_tag_x = tid_tag[tid_tag['tag'] == tag_x+1]
+        have_tag_x.reset_index(drop=False, inplace=True)
+        have_tag_y = tid_tag[tid_tag['tag'] == tag_y+1]
+        have_tag_y.reset_index(drop=False, inplace=True)
+        to_drop = have_tag_x.merge(have_tag_y, on='tid', how='inner', suffixes=('_x', '_y'))
+        tid_tag.drop(to_drop.index_y, inplace=True)
+
+def experimental_clean_remove2(tid_tag, matrix):
+    '''
+    If the correlation between tag-A and tag-B is higher than a certain threshold (which will determine
+    how 'matrix' is generated), remove tag-A from the tracks which have both tag-A and tag-B.
+
+    Parameters
+    ----------
+    tid_tag: pd.DataFrame
+        The tag_tag dataframe produced with create_tid_tag_table().
+    
+    matrix: np.ndarray
+        The two-dimensional correlation matrix produced with lastfm.Matrix.all_tag_is().
+
+    Examples
+    --------
+    If 'hard rock' is 80% correlated with 'rock', and threshold is less than 80%, take that 80% and remove the tag 'hard rock', leaving only that 20% of pure 'hard rock'.
+    '''
+    
+    for tag_x, tag_y in zip(*np.where(matrix>0)):
+        have_tag_x = tid_tag[tid_tag['tag'] == tag_x+1]
+        have_tag_x.reset_index(drop=False, inplace=True)
+        have_tag_y = tid_tag[tid_tag['tag'] == tag_y+1]
+        have_tag_y.reset_index(drop=False, inplace=True)
+        to_drop = have_tag_x.merge(have_tag_y, on='tid', how='inner', suffixes=('_x', '_y'))
+        tid_tag.drop(to_drop.index_x, inplace=True)
+
+if __name__ == '__main__':
 
     description = 'Script to generate a new LastFm database, similar in structure to the original LastFm database, containing only clean the tags for each track.'
     epilog = 'Example: python lastfm_cleaning.py ~/lastfm/lastfm_tags.db ~/lastfm/lastfm_tags_clean.db'
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
-    parser.add_argument("input", help="input db filename or path, or .csv folder path")
-    parser.add_argument("output", help="output db filename or path")
+    parser.add_argument("input", help="input .db filename or path, or .csv folder path")
+    parser.add_argument("output", help="output .db filename or path")
     parser.add_argument('--val', type=float, help="discard tags with val less than or equal to specified threshold")
     parser.add_argument('--supp-txt-path', help="set supplementary txt folder path")
+    parser.add_argument('--is-clean', action='store_true', help='skip preliminary tag cleaning (that is, if the input database is already clean)')
+    parser.add_argument('--matrix', help='if using --experimental, set the path to the saved matrix')
+    parser.add_argument('--exp-threshold', type=float, default=0.8, help='if using --experimental, set the correlation threshold to trigger the specified experimental function')
+    parser.add_argument('--exp', type=int, choices={1, 2, 3}, help='perform advanced operations on the database, using one of the following functions: \
+                                                                             1. if correlation between tag-A and tag-B is higher than a threshold, add tag-B to \
+                                                                             all the tracks which have tag-A but not tag-B; \
+                                                                             2. if correlation between tag-A and tag-B is higher than a threshold, remove tag-A \
+                                                                             from tracks which have both tag-A and tag-B; \
+                                                                             3. if correlation between tag-A and tag-B is higher than a threshold, remove tag-B \
+                                                                             from tracks which have both tag-A and tag-B')
     
     args = parser.parse_args()
 
-    # check if output filename ends with .db extension
-    if args.output[-3:] != '.db':
-        args.output += '.db'
+    assert os.path.splitext(args.output[-3:])[1] == '.db', 'output filename must have .db extension'
 
     # check if output file already exists
     if os.path.isfile(args.output):
-       raise OSError("file " + args.output + " already exists!")
+       raise FileNotFoundError("file " + args.output + " already exists!")
 
-    # check if different txt path has been provided
-    if args.supp_txt_path:
-        lastfm_utils.set_txt_path(args.supp_txt_path)
+    def clean_database():
+        # check if different txt path has been provided
+        if args.supp_txt_path:
+            lastfm_utils.set_txt_path(args.supp_txt_path)
 
-    # if user provided a .csv folder, load .csv into LastFm2Pandas; otherwise, load db into LastFm
-    if os.path.isdir(args.input):
-        lastfm = LastFm2Pandas.from_csv(args.input)
+        # check if user provided a .csv folder or a .db file (if using .csv, load .csv into LastFm2Pandas; otherwise, load .db into LastFm)
+        if os.path.isdir(args.input):
+            try:
+                tags = pd.read_csv(os.path.join(args.input, 'lastfm_tags.csv'))
+            except FileNotFoundError:
+                raise FileNotFoundError('Please make sure {} contains a file "lastfm_tags.csv".'.format(args.input))
+            try:
+                tids = pd.read_csv(os.path.join(args.input, 'lastfm_tids.csv'))
+            except FileNotFoundError:
+                raise FileNotFoundError('Please make sure {} contains a file "lastfm_tids.csv".'.format(args.input))
+            try:
+                tid_tag = pd.read_csv(os.path.join(args.input, 'lastfm_tid_tag.csv'))
+            except FileNotFoundError:
+                raise FileNotFoundError('Please make sure {} contains a file "lastfm_tid_tag.csv".'.format(args.input))
+            lastfm = LastFm2Pandas.load_from(tags=tags, tids=tids, tid_tag=tid_tag)
+        else:
+            lastfm = LastFm(args.input)
+
+        df = lastfm_utils.generate_final_df(lastfm)
+        df.reset_index(drop=True, inplace=True) # sanity check
+        df.index += 1
+
+        assert all(df.columns == ['tag', 'merge_tags']) # sanity check
+
+        # generate tables which will go into output database
+        tags = df['tag'].str.lower()
+        print('Matching all tags to the "clean" few ones...', end=' ', flush=True)
+        tag_tag = create_tag_tag_table(lastfm, df)
+        print('done')
+        print('Matching all tids to tags...', end=' ', flush=True)
+        tid_tag = create_tid_tag_table(lastfm, tag_tag, args.val)
+        print('done')
+        print('Purging tids...', end=' ', flush=True)
+        tids = tid_tag['tid'].drop_duplicates()
+        tids.index = tids.values
+        tids = tids.map(lastfm.tid_num_to_tid).reindex(pd.RangeIndex(1, len(lastfm.get_tid_nums())+1))
+        print('done')
+
+        return lastfm.LastFm2Pandas.load_from(tags=tags, tids=tids, tid_tag=tid_tag) # wrap into LastFm2Pandas class
+
+    if not args.exp:
+        clean_fm = clean_database()
     else:
-        lastfm = LastFm(args.input)
-    
-    df = lf_tool.generate_final_df(lastfm)
-    df.reset_index(drop=True, inplace=True) # shouldn't be needed to reset_index... this only adds extra safety
-    df.index += 1
+        from lastfm import Matrix
+        
+        if args.is_clean:
+            clean_fm = LastFm2Pandas(args.input) # does not support .csv
+        else:
+            clean_fm = clean_database()
+        
+        # if matrix has already been computed, load it; otherwise, compute it from scratch
+        if args.matrix:
+            matrix = Matrix.load_from(args.matrix)
+        else:
+            matrix = Matrix(clean_fm, dim=3, tags=None)
+        
+        correlation = matrix.all_tag_is(args.exp_threshold)
 
-    assert all(df.columns == ['tag', 'merge_tags'])
-
-    # generate tables which will go into output database
-    tags = df['tag'].str.lower()
-    print('Matching all tags to the "clean" few ones...', end=' ', flush=True)
-    tag_tag = create_tag_tag_table(lastfm, df)
-    print('done')
-    print('Matching all tids to tags...', end=' ', flush=True)
-    tid_tag = create_tid_tag_table(lastfm, tag_tag, args.val)
-    print('done')
-    print('Purging tids...', end=' ', flush=True)
-    tids = tid_tag['tid'].drop_duplicates()
-    tids.index = tids.values
-    tids = tids.map(lastfm.tid_num_to_tid).reindex(pd.RangeIndex(1, len(lastfm.get_tid_nums())+1))
-    print('done')
-
-    # generate output
-    print('Saving new tables as a db file...', end=' ', flush=True)
+        if args.exp == 1:
+            clean_fm.tid_tag = experimental_clean_add(clean_fm.tid_tag, correlation) # the function experimental_clean_add() is not inplace; the two functions experimental_clean_remove() are
+        elif args.exp == 2:
+            experimental_clean_remove1(clean_fm.tid_tag, correlation)
+        elif args.exp == 3:
+            experimental_clean_remove2(clean_fm.tid_tag, correlation)
+        
+    # save into a new .db file
+    print('Saving new tables as a .db file...', end=' ', flush=True)
     conn = sqlite3.connect(args.output)
-    tags.to_sql('tags', conn, index=False)
-    tids.to_sql('tids', conn, index=False)
-    tid_tag.to_sql('tid_tag', conn, index=False)
+    fm.tags.to_sql('tags', conn, index=False)
+    fm.tids.to_sql('tids', conn, index=False)
+    fm.tid_tag.to_sql('tid_tag', conn, index=False)
     print('done')
     conn.close()
